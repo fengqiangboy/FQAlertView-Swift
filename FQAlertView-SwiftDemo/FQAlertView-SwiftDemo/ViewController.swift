93bb42b0f337bbf89b6ff6f08ea966f743c21eff
FengQiang <498460828@qq.com>
2016-10-25 09:58:34 +0800

    Add Demo

---
+++ FQAlertView-SwiftDemo/FQAlertView-SwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FQAlertView-SwiftDemo
+//
+//  Created by 奉强 on 16/10/25.
+//  Copyright © 2016年 奉强. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
